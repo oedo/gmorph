ab935d36914b89569a702ef8db9aa56ee17758a2
aono <aono@astom.co.jp>
2020-09-07 19:20:07 +0900

    resolve uncompilable problem on debug mode.

--- src/harmonic.cpp
+++ src/harmonic.cpp
@@ -9,11 +9,11 @@
 
 #include "StdAfx.h"
 
-#ifdef _DEBUG
-#define new DEBUG_NEW
-#undef THIS_FILE
-static char THIS_FILE[] = __FILE__;
-#endif
+//#ifdef _DEBUG
+//#define new DEBUG_NEW
+//#undef THIS_FILE
+//static char THIS_FILE[] = __FILE__;
+//#endif
 
 #include <math.h>
 #include "smd.h"
